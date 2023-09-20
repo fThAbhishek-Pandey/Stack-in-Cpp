7d37051347f293937ce2e15de74fae46f1161cc0
Abhishek Pandey <121682915+fThAbhishek-Pandey@users.noreply.github.com>
2023-09-20 15:08:54 +0530

    remove bottom element using loop

---
+++ removebott.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+void display (stack<int>st){
+    while (!st.empty()){
+        cout<<st.top()<<" ";
+        st.pop();
+    }
+    cout<<endl;
+    return;
+}
+  void removebott (stack<int>& st){
+    stack<int>temp;
+    while (!st.empty()){
+        int curr= st.top();
+        st.pop();
+        temp.push(curr);
+    }
+    temp.pop();
+    while(!temp.empty()){
+       int curr=temp.top();
+       temp.pop();
+       st.push(curr);
+    }
+    return;
+  }
+int main (){
+    cout<<"enter the size of stack : ";
+    int n;
+    cin>>n;
+    cout<<"enter the element of stack\n ";
+    stack<int> st;
+    for (int i=0;i<n;i++){
+        int ele ;
+        cin>>ele;
+        st.push (ele);
+    }
+    display(st);
+    removebott(st);
+    display (st);
+    return 0;
+}
